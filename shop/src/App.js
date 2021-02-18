62fd751a04e4cc572db0b5ae0aca49ea02a1d7a8
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-02-18 12:48:30 +0200

    added state for counter with hook

--- shop/src/App.js
+++ shop/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <>
       {/* <Header />
       <Main /> */}
-      <Counter />
+      <Counter title="We test our counter"/>
     </>
   );
 };
--- shop/src/components/Counter/Counter.js
+++ shop/src/components/Counter/Counter.js
@@ -1,20 +1,104 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import classes from "./Counter.module.css";
 
-class Counter extends Component {
-  render() {
-    return (
-      <div className={classes.Container}>
-        <h1>Counter</h1>
-        <div className={classes.Count}>0</div>
-        <div>
-          <button>-</button>
-          <button>0</button>
-          <button>+</button>
-        </div>
+const initialState = {
+  sum: 700,
+  name: "Bob",
+};
+
+const zero = 0;
+
+const Counter = ({ title }) => {
+  const [count, setCount] = useState(zero);
+  const [name, setName] = useState(title);
+  const [adress, setAdress] = useState("Kyiv");
+
+  const plus = () => {
+    setCount(count + 1);
+  };
+
+  const reset = () => {
+    setCount(zero);
+  };
+  const minus = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className={classes.Container}>
+      <h1>{name}</h1>
+      <div className={classes.Count}>{count}</div>
+
+      <div>
+        <button onClick={minus}>-</button>
+        <button onClick={reset}>0</button>
+
+        <button onClick={plus}>+</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Counter;
+
+// import React, { Component } from "react";
+// import classes from "./Counter.module.css";
+
+// class Counter extends Component {
+//   // constructor(){
+//   //   super();
+//   //   this.state = {
+//   //     count: 0,
+//   //   }
+//   //   this.plus =this.plus.bind(this)
+//   // }
+
+//   state = {
+//     count: 0,
+//     name: "Mar",
+//     age: 25,
+//   };
+//   // plus = () => {
+//   //   this.state.count = 30;
+//   // }
+
+//   plus =  () => {
+//      this.setState((prevState) => ({ count: prevState.count + 1 }));
+//     console.log(this.state.count);
+//   };
+
+//   reset = () => {
+//     this.setState({ count: 0 });
+//   };
+
+//   // plus = () => {
+//   //   // console.log(this.state.count);
+//   //   this.setState({count: this.state.count + 1})
+//   //   console.log(this.state.count);
+//   // }
+
+//   minus = () => {
+//     this.setState((prevState) => ({ count: prevState.count - 1 }));
+//   };
+
+//   render() {
+//     const { title } = this.props;
+// return (
+//   <div className={classes.Container}>
+//     <h1>{title}</h1>
+//     <div className={classes.Count}>{this.state.count}</div>
+
+//     <div>
+//       <button onClick={this.minus}>-</button>
+//       {/* <button onClick={()=>console.log('hi')}>0</button> */}
+//       <button onClick={this.reset}>0</button>
+
+//       <button onClick={this.plus}>+</button>
+//       {/* <button onClick={this.plus.bind(this)}>+</button> */}
+//     </div>
+//   </div>
+// );
+//   }
+// }
+
+// export default Counter;
