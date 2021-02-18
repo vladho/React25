c58dd7db1d59f6bcdb699e68e47bbc582f8b4729
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-02-18 09:34:11 +0200

    added button from Material-ui

--- shop/src/components/Main/Main.js
+++ shop/src/components/Main/Main.js
@@ -2,6 +2,8 @@ import React from "react";
 import CardCollection from "../CardCollection/CardCollection";
 import FilterBlock from "../FilterBlock/FilterBlock";
 import Section from "../Section/Section";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import "./Main.css";
 
 const Main = () => {
@@ -9,7 +11,12 @@ const Main = () => {
     <main className="main">
       <FilterBlock>
         <Section />
+        <Button variant="contained">Default</Button>
+      <Button variant="contained" color="secondary">
+        Primary
+      </Button>
       </FilterBlock>
+      
       <CardCollection />
     </main>
   );
