0cf60708b21780979a8d6fdad86d3a8b54ad0bdf
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-02-22 12:29:35 +0200

    clear Form

--- todo/src/components/Form/Form.js
+++ todo/src/components/Form/Form.js
@@ -1,81 +1,25 @@
 import React, { Component } from "react";
 import "./Form.css";
-class Form extends Component {
-  
-  formInitialState={
-    title: '',
-    author: '',
-    priority: 'Low',
-    agree: false,
-    status:false
-  }
-  
-  state={
-    title: '',
-    author: '',
-    priority: 'Low',
-    agree: false,
-    status:false
-  }
-// inputTitleHandler =(e)=>{
-// const input = e.target;
-// const value = input.value;
-// this.setState({
-//   title: value
-// })
-// }
-// inputAuthorHandler =({target})=>{
-//   const {value} = target
-//   this.setState({
-//     author: value
-//   })
-//   }
-
-  inputHandler =({target})=>{
-    const {value,name,type,checked} = target;
-    this.setState({
-      [name]:type==='checkbox' ? checked : value
-    })
-  }
-
-  submitHeandler =(e)=>{
-    const {title,author,priority,status}=this.state
-    e.preventDefault();
-    const singleTask= {
-      title,
-      author,
-      priority,
-      id: Date.now(),
-      status
-    }
-    this.props.addTask(singleTask)
-    this.setState({
-      ...this.formInitialState
-    })
-
-  }
-
-  render() {
-    const {title,author,priority,agree}=this.state
+const Form =() => {
     return (
-      <form  onSubmit={this.submitHeandler} className="NewTodoForm"autoComplete="off">
+      <form   className="NewTodoForm"autoComplete="off">
         <input
-        onChange={this.inputHandler}
+        
           className="NewTodoForm__name"
           type="text"
           name="title"
           placeholder="New Todo"
-          value={title}
+          value="title"
         />
         <input
-        onChange={this.inputHandler}
+        
           className="NewTodoForm__name"
           type="text"
           name="author"
           placeholder="Author"
-          value={author}
+          value='Author'
         />
-        <select onChange={this.inputHandler} value={priority} name="priority" className="NewTodoForm__select">
+        <select  value="Low" name="priority" className="NewTodoForm__select">
           <option value='' disabled hidden>
             Priority
           </option>
@@ -84,7 +28,7 @@ class Form extends Component {
           <option value="High">High</option>
         </select>
         <label htmlFor="agree" className="confirm">
-          <input onChange={this.inputHandler} type="checkbox" id="agree" name="agree" checked={agree}/>
+          <input  type="checkbox" id="agree" name="agree" checked=""/>
           Agree with our policy
         </label>
         <button className="NewTodoForm__submit" type="submit">
@@ -92,7 +36,7 @@ class Form extends Component {
         </button>
       </form>
     );
-  }
+  
 }
 
 export default Form;
