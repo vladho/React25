e87af63b5e1fa337e8f5ba1beb1bfe36263bd570
vladho <vladhopilo@gmail.com>
2021-02-24 10:21:09 +0200

    Revert "up"
    This reverts commit bea40eaf5394c599db3c7a11ba15009d46c5113f, reversing
    changes made to 1a6166bcb3987b7d87fb43cab889dbcf15a493a5.

---
+++ todo/src/components/Form/Form.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react"
+import { v4 as uuidv4 } from "uuid"
+import "./Form.css"
+
+class Form extends Component {
+  initialState = {
+    title: "",
+    author: "",
+    priority: "Low",
+    agree: false,
+    // status: false,
+  }
+
+  state = {
+    ...this.initialState,
+    // title: "",
+    // author: "",
+    // priority: "Low",
+    // agree: false,
+  }
+
+  inputHeandler = ({ target }) => {
+    // console.log(e);
+    // const input = e.target;
+    // const value = input.value;
+    // const name = input.name;
+    const { value, name, type } = target
+    this.setState({
+      [name]: type === "checkbox" ? !this.state.agree : value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.agree) {
+    }
+    const singleTask = {
+      title: this.state.title,
+      author: this.state.author,
+      priority: this.state.priority,
+      status: false,
+      id: uuidv4(),
+    }
+    console.log(singleTask)
+    this.props.addToList(singleTask)
+
+    this.setState({ ...this.initialState })
+  }
+
+  // titleInputHeandler = ({ target }) => {
+  //   const { value } = target;
+  //   // const value = input.value;
+  //   this.setState({
+  //     title: value,
+  //   });
+  // };
+
+  // authorInputHeandler = (e) => {
+  //   const input = e.target;
+  //   const value = input.value;
+  //   this.setState({
+  //     author: value,
+  //   });
+  // };
+
+  render() {
+    const { title, author, priority, agree } = this.state
+    return (
+      <form onSubmit={this.handleSubmit} className="NewTodoForm" autoComplete="off">
+        <input onChange={this.inputHeandler} className="NewTodoForm__name" type="text" name="title" placeholder="New Todo" value={title} />
+        <input onChange={this.inputHeandler} className="NewTodoForm__name" type="text" name="author" placeholder="Author" value={author} />
+        <select onChange={this.inputHeandler} value={priority} name="priority" className="NewTodoForm__select">
+          <option value="" disabled hidden>
+            Priority
+          </option>
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+        <label htmlFor="agree" className="confirm">
+          <input onChange={this.inputHeandler} type="checkbox" id="agree" name="agree" checked={agree} />
+          Agree with our policy
+        </label>
+        <button disabled={!agree} className={agree ? "NewTodoForm__submit" : "NewTodoForm__submit-inactive"} type="submit">
+          Add Todo
+        </button>
+      </form>
+    )
+  }
+}
+
+export default Form
---
+++ todo/src/components/Todo/Todo.js
@@ -0,0 +1,24 @@
+import React from "react"
+import "./Todo.css"
+const Todo = ({ title, author, priority, status, id, removeFromList, editStatusTask }) => {
+  return (
+    <div className="Todo">
+      <div
+        onClick={() => {
+          editStatusTask(id)
+        }}
+      >
+        <p className={`Todo__name ${status && "done"}`}>{title}</p>
+        <p className={`Todo__priority ${status && "done"}`}> {priority}</p>
+        <p className={`Todo__author   ${status && "done"}`}>{author}</p>
+      </div>
+      <span onClick={() => removeFromList(id)} className="Todo__remove">
+        X
+      </span>
+    </div>
+  )
+}
+
+export default Todo
+
+// ${status && "done"}
